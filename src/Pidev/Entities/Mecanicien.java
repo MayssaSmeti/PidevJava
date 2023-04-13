088bfd80cebfe2fc50103e91ed428825d53ae5fb
mayssa21 <mayssa.smeti@esprit.tn>
2023-04-13 23:39:42 +0200

    first commit

---
+++ src/Pidev/Entities/Mecanicien.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pidev.Entities;
+
+/**
+ *
+ * @author Mayssa
+ */
+public class Mecanicien  extends User {
+    
+}
---
+++ src/Pidev/Utilis/IService.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pidev.Utilis;
+
+/**
+ *
+ * @author Mayssa
+ */
+public interface IService  {
+    
+}
